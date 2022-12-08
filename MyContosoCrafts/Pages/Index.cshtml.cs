27a192ba9bebacdc4a573f07e32d9814228a6bc7
Marcin Kolenda <marcinkolenda419@gmail.com>
2022-12-08 14:05:46 +0100

    Add project files.

---
+++ MyContosoCrafts/Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MyContosoCrafts.Models;
+using MyContosoCrafts.Services;
+
+namespace MyContosoCrafts.Pages;
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+    public IEnumerable<Product> Products { get; private set; }
+    public JsonFileProductService ProductService { get; private set; }
+
+    public IndexModel(
+        ILogger<IndexModel> logger,
+        JsonFileProductService productService)
+    {
+        _logger = logger;
+        ProductService = productService;
+    }
+
+    public void OnGet()
+    {
+        Products = ProductService.GetProducts();
+    }
+}
---
+++ MyContosoCrafts/Services/JsonFileProductService.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json;
+using MyContosoCrafts.Models;
+
+namespace MyContosoCrafts.Services;
+
+public class JsonFileProductService
+{
+
+    public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
+    {
+        WebHostEnvironment = webHostEnvironment;
+    }
+
+    public IWebHostEnvironment WebHostEnvironment { get; }
+
+    private string JsonFilePath => Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json");
+
+    public IEnumerable<Product> GetProducts()
+    {
+        using var jsonFileReader = File.OpenText(JsonFilePath);
+        return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+            options: new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            });
+    }
+}
